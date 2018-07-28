c2ac1aba8c124141abaf16e7d27bfa4a0084bc53
ImAndreLeandro <40339453+ImAndreLeandro@users.noreply.github.com>
2018-07-28 09:46:25 +0100

    Create Vanish.java

---
+++ net/imandreleandro/vanishstaffsimple/Vanish.java
@@ -0,0 +1,11 @@
+package net.imandreleandro.vanishstaffsimple;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+
+public class Vanish {
+	
+	public static ArrayList<Player> list = new ArrayList<>();
+	
+}
